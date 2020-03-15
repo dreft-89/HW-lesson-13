d1fac95d21bd29adeb4024b2b8906bfd94fd269b
Uzun Igor <te10@mail.ru>
2020-03-15 19:42:19 +0200

    make modification on JS, rename discr to getDiscriminant, D to discriminant and D==0 to discriminant ===0

--- scrpt.js
+++ scrpt.js
@@ -1,17 +1,18 @@
-function des(a,b,c) {
-var D = b * b - 4 * a * c;
-var x1;
-var x2;
-alert(D);    // функцию на JS вычисляющая дискриминант
-if (D > 0) {
+function getDiscriminant(a,b,c) {
+    var discriminant = b * b - 4 * a * c;
+    var x1;
+    var x2;
+    alert(discriminant);   
+    if (discriminant > 0) {
     x1 = (-b + Math.sqrt(D)) / (2 * a);
     x2 = (-b - Math.sqrt(D)) / (2 * a);
-    alert('x1 = ' + x1 +";  " + 'x2= ' + x2);
-} else if (D==0) {
+    alert("x1 = " + x1 +";  " + "x2= " + x2);
+    } else if (discriminant===0) {
     x1 = (-b) / (2*a);
-    alert('x= '+x1);
-} else if (D < 0) {
+    alert("x= " +x1);
+    } else if (discriminant < 0) {
     alert("Нет действительных корней");
 }
 }
-des(10, -2, 6);  //корни квадратного уравнения
+
+getDiscriminant(10, -2, 6); 
