9438d0f97415567ccfc5d12b15ed4820e7ce4757
Uzun Igor <te10@mail.ru>
2020-03-17 18:43:35 +0200

    formating JS

--- scrpt.js
+++ scrpt.js
@@ -1,18 +1,18 @@
-function getDiscriminant(a,b,c) {
+function getDiscriminant(a, b, c) {
     var discriminant = b * b - 4 * a * c;
     var x1;
     var x2;
-    alert(discriminant);   
+    alert(discriminant);
     if (discriminant > 0) {
-    x1 = (-b + Math.sqrt(D)) / (2 * a);
-    x2 = (-b - Math.sqrt(D)) / (2 * a);
-    alert("x1 = " + x1 +";  " + "x2= " + x2);
-    } else if (discriminant===0) {
-    x1 = (-b) / (2*a);
-    alert("x= " +x1);
+        x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+        x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+        alert("x1 = " + x1 + ";  " + "x2= " + x2);
+    } else if (discriminant === 0) {
+        x1 = (-b) / (2 * a);
+        alert("x= " + x1);
     } else if (discriminant < 0) {
-    alert("Нет действительных корней");
-}
+        alert("Нет действительных корней");
+    }
 }
 
-getDiscriminant(10, -2, 6); 
+getDiscriminant(10, -2, 6);
